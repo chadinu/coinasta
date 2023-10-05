07739820915e30b584046e440fd341754847e089
chadinu <green.cryptodev@gmail.com>
2023-10-04 22:20:54 -0700

    fix unsigned_char error in chainparams

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -187,7 +187,7 @@ public:
 
         base58Prefixes[PUBKEY_ADDRESS] = list_of(0);
         base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
-        base58Prefixes[SECRET_KEY] = list_of(128);
+        base58Prefixes[SECRET_KEY] = list_of(129);
     }
 
     virtual bool RequireRPCPassword() const { return false; }
